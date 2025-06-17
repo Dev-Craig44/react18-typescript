3c085c1dc55eb775f0a78adc02da46537700c648
Craig Kelly <105810414+Dev-Craig44@users.noreply.github.com>
2025-06-17 11:07:12 -0400

    🧼 clean comments

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,9 @@ import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 import ExpenseForm from "./expense-tracker/components/ExpenseForm";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
-// 12.) export this so other modules can use it
-// 11.) retrieve categories (real world, we would retrieve this from an API)
 export const categories = ["Utilities", "Health", "Food"];
+
 function App() {
-  // use stateHook to store the list of expenses
   const [selectedCategory, setSetselectedCategory] = useState("");
   const [expenses, setExpenses] = useState([
     { id: 1, description: "Groceries", amount: 50.0, category: "Food" },
@@ -20,15 +18,12 @@ function App() {
     { id: 3, description: "Gym Membership", amount: 30.0, category: "Health" },
   ]);
 
-  //  the reason we are not using a state variable for the visible expenses is because this is redundant, because this is something that we can calculate from the existing state variables
-  // don't use state variables for things we can compute from the existing state variables
   const visibleExpenses = selectedCategory
     ? expenses.filter((e) => e.category === selectedCategory)
     : expenses;
 
   return (
     <div>
-      {/* 17.) add expense form inside a div.mb-5 */}
       <div className="mb-5">
         <ExpenseForm />
       </div>
@@ -37,10 +32,8 @@ function App() {
           onSelectCategory={(category) => setSetselectedCategory(category)}
         />
       </div>
-      {/* add the expense list */}
       <ExpenseList
         expenses={visibleExpenses}
-        // to delete we are going to pass a callback function that takes an id and calls {setExpenses}, passing a filtered list of expenses, with the expression being the we are going to pick eveything but this expense with the id
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
--- src/expense-tracker/components/ExpenseFilter.tsx
+++ src/expense-tracker/components/ExpenseFilter.tsx
@@ -1,12 +1,9 @@
-// 13.) import categories from App
 import { categories } from "../../App";
 
 interface Props {
   onSelectCategory: (category: string) => void;
 }
 
-// This component is purely responsible for showing filters
-// The act of filtering will be done in our App component, because that is where we maintain the state of the expenses
 const ExpenseFilter = ({ onSelectCategory }: Props) => {
   return (
     <select
@@ -14,7 +11,6 @@ const ExpenseFilter = ({ onSelectCategory }: Props) => {
       onChange={(event) => onSelectCategory(event.target.value)}
     >
       <option value="">All categories</option>
-      {/* 14.) dynamically render the options */}
       {categories.map((c) => (
         <option key={c} value={c}>
           {c}
